f57c69cf6f0661a258f7e947502e0682f505dd5a
dfmonteiro2 <dfmonteiro@Douglass-MacBook-Pro.local>
2015-03-23 12:02:48 -0400

    paint
    paint

--- MassPaint/draw.js
+++ MassPaint/draw.js
@@ -1,3 +1,4 @@
+speed (0)
 record();
 reset();
 hide();
@@ -7,41 +8,181 @@ $("#button-save").on("click",function() {
 });
 
 $("#button-replay").on("click",function() {
+ speed (300)
  replay();
+ speed (0)  // why 2 different speeds? //
 });
 
 $("#button-eraser").on("click",function() {
 	$(this).addClass("selected");
-	$("button-pen").removeClass("selected");
-
-	tool = "eraser"
+	tool = "eraser";
 	lineWidth(30);
-	lineColor("white")
+	lineColor("white");
 });
 
-$("#button-pen").on("click",function() {
+$("#button-color-tan").on("click",function() {
 	$(this).addClass("selected");
-	$("button-eraser").removeClass("selected");
-
+	tool = "brush-tan";
+	lineWidth(15);
+	lineColor("tan");
 });
 
-tool = "earser";
+$("#button-color-blue").on("click",function() {
+	$(this).addClass("selected");
+	tool = "brush-blue";
+	lineWidth(15);
+	lineColor("blue");
+});
+
+$("#button-color-green").on("click",function() {
+	$(this).addClass("selected");
+	tool = "brush-green";
+	lineWidth(15);
+	lineColor("green");
+});
+
+$("#button-color-brown").on("click",function() {
+	$(this).addClass("selected");
+	tool = "brush-brown";
+	lineWidth(15);
+	lineColor("brown");
+});
+
+$("#button-triangle").on("click",function() {
+  $(".tool.selected").removeClass("selected");
+  $(this).addClass("selected");
+  tool = "triangle";
+ lineWidth(width);
+ lineColor(color);
+});
+
+$("#button-shape").on("click",function() {
+  $(".tool.selected").removeClass("selected");
+  $(this).addClass("selected");
+  tool = "shape";
+ lineWidth(width);
+ lineColor(color);
+});
+
+
+/* $("#button-crab").on("click",function() {
+  $(".tool.selected").removeClass("selected");
+  $(this).addClass("selected");
+  tool = "crab";
+ lineWidth(width);
+ lineColor(color);
+});
+*/
+
+function triangle() {
+  angle(0);
+  forward(50);
+  rotate(120);
+  forward(50);
+  rotate(120);
+  forward(50);
+  rotate(120);
+}
+
+function shape() {
+  angle(0);
+  forward(20);
+  rotate(72);
+  forward(20);
+  rotate(72);
+  forward(20);
+  rotate(72);
+  forward(20);
+  rotate(72);
+  forward(20);
+  rotate(72);
+}
+
+/* function crab() {
+
+box-shadow:
+    0 0 0 1em red,
+    0 1em 0 1em red,
+    -2.5em 1.5em 0 .5em red,
+    2.5em 1.5em 0 .5em red,
+    -3em -3em 0 0 red,
+    3em -3em 0 0 red,
+    -2em -2em 0 0 red,
+    2em -2em 0 0 red,
+    -3em -1em 0 0 red,
+    -2em -1em 0 0 red,
+    2em -1em 0 0 red,
+    3em -1em 0 0 red,
+    -4em 0 0 0 red,
+    -3em 0 0 0 red,
+    3em 0 0 0 red,
+    4em 0 0 0 red,
+    -5em 1em 0 0 red,
+    -4em 1em 0 0 red,
+    4em 1em 0 0 red,
+    5em 1em 0 0 red,
+    -5em 2em 0 0 red,
+    5em 2em 0 0 red,
+    -5em 3em 0 0 red,
+    -3em 3em 0 0 red,
+    3em 3em 0 0 red,
+    5em 3em 0 0 red,
+    -2em 4em 0 0 red,
+    -1em 4em 0 0 red,
+    1em 4em 0 0 red,
+    2em 4em 0 0 red;
+
+    background: red;
+    width: 1em;
+    height: 1em;
+    overflow: hidden;
+*/
 
 var drawing = false;
-var erasing = false;
 var tool = "pen";
 
 var color="black";
-var width=1;
+var width=4;
 
 $(document).on("mousedown",function(e) {
   e.preventDefault();
   moveTo(e.pageX,e.pageY);
   if(tool == "pen" || tool == "eraser") { 
     drawing = true;
-  }
+  } else if(tool == "triangle") {
+    triangle();
+  } else if(tool == "brush-tan")
+    drawing = true;
 });
 
+$(document).on("mousedown",function(e) {
+  e.preventDefault();
+  moveTo(e.pageX,e.pageY);
+  if(tool == "shape")
+    shape();
+});
+	
+$(document).on("mousedown",function(e) {
+  e.preventDefault();
+  moveTo(e.pageX,e.pageY);
+  if(tool == "brush-blue")
+    drawing = true;
+});
+	  
+$(document).on("mousedown",function(e) {
+  e.preventDefault();
+  moveTo(e.pageX,e.pageY);
+  if(tool == "brush-green")
+    drawing = true;
+});
+  
+$(document).on("mousedown",function(e) {
+  e.preventDefault();
+  moveTo(e.pageX,e.pageY);
+  if(tool == "brush-brown")
+    drawing = true;
+});  
+  
 $(document).on("mousemove",function(e) {
   e.preventDefault();
   if(drawing) {
