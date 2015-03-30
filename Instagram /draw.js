3a5fb277a51fff04fa6eb4875602621f2d0af6eb
dfmonteiro2 <dfmonteiro@Douglass-MacBook-Pro.local>
2015-03-30 14:03:05 -0400

    everything
    everything

--- Instagram /draw.js
+++ Instagram /draw.js
@@ -16,3 +16,5 @@ addPerson("Frank Scott","San Diego, CA", "http://lorempixel.com/200/200/people/3
 addPerson("Amy Smith","New York, NY", "http://lorempixel.com/200/200/people/4/");
 addPerson("Amy Smith","New York, NY", "http://lorempixel.com/200/200/people/4/");
 
+addPerson("http://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/s150x150/e15/11055442_1387234548264057_209258360_n.jpg",
+        "https://instagram.com/p/0lS-LWTbw9/", "432");
