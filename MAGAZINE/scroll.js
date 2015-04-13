73e2c5344f37f8e3a4608d0d38d0ef10b3b735c8
dfmonteiro2 <dfmonteiro@Douglass-MacBook-Pro.local>
2015-04-13 03:46:35 -0400

    mag
    mag

--- MAGAZINE/scroll.js
+++
@@ -1,9 +0,0 @@
-$(".scrolling").scrollFlight();
-
-$("h1").on("arrived",function(e) {
-  $(this).addClass("on-page");
-});
-
-$(".iframe-cover").on("arriving",function(e) {
-  $(this).addClass("appeared");
-});
